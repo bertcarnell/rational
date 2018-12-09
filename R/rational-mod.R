91ffc2355d6d5c1c40e73fb84e763aec4ee35703
Rob Carnell <bertcarnell@gmail.com>
2018-12-09 13:37:06 -0500

    Added mod tests.  Added mod S3 and R6 functions

--- R/rational-mod.R
+++ R/rational-mod.R
@@ -4,6 +4,18 @@
 
 #' @rdname rational-operators
 #' @export
+#' @examples
+#' a <- rational(5L, 1L, "S4")
+#' b <- rational(2L, 1L, "S4")
+#' d <- rational(3L, 2L, "S4")
+#' e <- rational(1L, 2L, "S4")
+#' stopifnot(rational(1L, 1L, "S4") == a %% b)
+#' stopifnot(rational(0L, 1L, "S4") == d %% e)
+#' stopifnot(rational(1L, 1L, "S4") == 5L %% b)
+#' stopifnot(rational(1L, 1L, "S4") == a %% 2L)
+#' stopifnot(rational(0L, 1L, "S4") == 2L %% e)
+#' stopifnot(1 == 5 %% b)
+#' stopifnot(1 == a %% 2)
 setMethod("%%", signature = c("rationalS4", "rationalS4"),
           function(e1,e2)
           {
@@ -46,3 +58,41 @@ setMethod("%%", signature = c("rationalS4", "numeric"),
             e1 - e2 * e1 %/% e2
           }
 )
+
+#' @rdname rational-operators
+#' @export
+#' @examples
+#' a <- rational(5L, 1L, "S3")
+#' b <- rational(2L, 1L, "S3")
+#' d <- rational(3L, 2L, "S3")
+#' e <- rational(1L, 2L, "S3")
+#' stopifnot(rational(1L, 1L, "S3") == a %% b)
+#' stopifnot(rational(0L, 1L, "S3") == d %% e)
+#' stopifnot(rational(1L, 1L, "S3") == 5L %% b)
+#' stopifnot(rational(1L, 1L, "S3") == a %% 2L)
+#' stopifnot(rational(0L, 1L, "S3") == 2L %% e)
+#' stopifnot(1 == 5 %% b)
+#' stopifnot(1 == a %% 2)
+'%%.rationalS3' <- function(e1, e2)
+{
+  e1 - e2 * e1 %/% e2
+}
+
+#' @rdname rational-operators
+#' @export
+#' @examples
+#' a <- rational(5L, 1L, "R6")
+#' b <- rational(2L, 1L, "R6")
+#' d <- rational(3L, 2L, "R6")
+#' e <- rational(1L, 2L, "R6")
+#' stopifnot(rational(1L, 1L, "R6") == a %% b)
+#' stopifnot(rational(0L, 1L, "R6") == d %% e)
+#' stopifnot(rational(1L, 1L, "R6") == 5L %% b)
+#' stopifnot(rational(1L, 1L, "R6") == a %% 2L)
+#' stopifnot(rational(0L, 1L, "R6") == 2L %% e)
+#' stopifnot(1 == 5 %% b)
+#' stopifnot(1 == a %% 2)
+'%%.rationalR6' <- function(e1, e2)
+{
+  e1 - e2 * e1 %/% e2
+}
